cabc9a69c6f49f6af2bd3d48e0dbd07cf5d18cf9
“Aaron-Marsh” <aaronanthonymarsh@gmail.com>
2022-06-24 16:20:51 +0100

    setup redux files and navigation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <h1 aria-label="main-title">Hello</h1>
+    <>
+      <h1 aria-label="main-title">Hello</h1>
+      <main>
+                <Routes>
+                    <Route path="/" element={
+                            <Welcome />
+                    }/>
+                    <Route path="/setup" element={
+                            <Setup />
+                    }/>
+                    <Route path="/quiz" element={
+                            <Quiz />
+                    }/>
+                    <Route path="/leaderboards" element={
+                            <Leaderboards />
+                    }/>
+                </Routes>
+      </main>
+    </>
   );
 }
 
