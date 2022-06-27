b7ed22be7eeee0ab5525021ab79b0b101da6e08c
“Aaron-Marsh” <aaronanthonymarsh@gmail.com>
2022-06-27 13:21:44 +0100

    add call for questions from socket.io

--- src/components/Question/index.js
+++ src/components/Question/index.js
@@ -1,9 +1,54 @@
-import react from 'react';
+import react, { useEffect, useState } from 'react';
+import { io } from 'socket.io-client'
+const socket = io('http://localhost:4000');
 
-const Question = () => {
-    return (
-        <>
+io({query: { name: 'Sally'}})
+
+socket.emit('start', {category: 2, difficulty: 'easy', questionsAmount: 12})
+let questionData={category:'blank'};
+
+socket.on('ready', (data) => {
+    questionData = data;
+    console.log(questionData)
+})
+    
+    
+    const Question = () => {
+        const [score, setScore] = useState(0);
+        const [questions, setQuestions] = useState(questionData)
+
+
+        useEffect(() => {
+            setQuestions(questionData)
+        })
+        
+        const newQuestion = (e) => {
+            e.preventDefault()
+            socket.emit('retrieveQuestion', {questionScore: score})
+            setQuestions(questionData)
+            
+        }
+
+        const listAnswers = () => {
+
+        }
+        
+        // useEffect(() => {
+            //     socket.on('noQuestionsLeft', (data) => {
+                //         console.log('data'+data.score)
+                //     })
+                // })
+                
+                return (
+                    
+                    <>
         <h2>Questions</h2>
+        <h3>{questions.question}</h3>
+        <ul>
+            <li>{questions.correct_answer}</li>
+        </ul>
+        
+        <button onClick={newQuestion}>hello</button>
         </>
     )
 }
