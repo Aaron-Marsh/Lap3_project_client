55609a95908dbb725b7f3d7d203dcb5b21cf367b
“millman97” <thomas.millington@mail.com>
2022-06-27 11:07:54 +0100

    add leaderboard folders

---
+++ src/pages/Leaderboards/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Leaderboards() {
+  return (
+    <div className='page leaderboards'>
+        <h1>Leaderboards</h1>
+        
+    </div>
+  )
+}
+
+export default Leaderboards
---
+++ src/pages/index.js
@@ -0,0 +1,4 @@
+
+
+
+export { default as Leaderboards } from './Leaderboards';
