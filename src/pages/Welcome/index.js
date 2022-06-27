1ee3fbd4381922a2be858102e1981275531bc31c
GMillerMc <gmiller04@yahoo.com>
2022-06-27 17:17:30 +0100

    push changes to page

--- src/pages/Welcome/index.js
+++ src/pages/Welcome/index.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState} from 'react';
 import styles from './index.module.css'
 
 
@@ -20,7 +20,7 @@ function Welcome() {
           <div className='text'>
             <p>Welcome to About Time. The quiz game you can play on your own or with friends. If you want to jump straight into setting up a game click the 'Got it!' button below.</p>
 
-            <p>If it's your first time here or you need a refresher on how to play then click the 'Help' button below.</p>
+            <p>If it's your first time here or you need a refresher on how to play then click the 'Help' button below. Click help again or anywhere outside the displayed text to close the help section.</p>
           </div>
 
           <div className='buttons'>
@@ -29,18 +29,34 @@ function Welcome() {
               
               <Popup open={open} closeOnDocumentClick onClose={closeModal}>
                 <div className="modal">
-                  <a className="close" onClick={closeModal}>
-                  
+                  <a className="close" onClick={closeModal}>Close
                   </a>
                     <h3>Need a hand? 🕒</h3>
 
                     <h4>Starting a game</h4>
-                    <p>Enter your username then select how many players, how hard you would like the quiz to be and the topic.</p>
+                    <ul>
+                      <li>Enter your username</li>
+                      <li>Select how many players</li>
+                      <li>Choose a difficulty</li>
+                      <li>Choose a topic</li>
+                      <li>Choose the amount of questions</li>
+                      <li>Start the quiz!</li>
+                    </ul>
 
                     <h4>Playing the game</h4>
-                    <p>The question will be displayed on your screen, at which point you will be given 10 seconds to choose an answer. Once you choose an answer the tile will either flash green for correct or red for incorrect.
-                    The amount of points you are awarded for a correct answer is calculated by our very clever algorithm which takes the time left and multiplies by a secret value.</p>
+                    <p>The question will be displayed on your screen, at which point you will be given 10 seconds to choose an answer. 
+                    
+                    Once you choose an answer the tile will either flash green for correct or red for incorrect.
+                    
+                    The amount of points you are awarded for a correct answer is calculated by our very clever algorithm which takes the time left and multiplies by a secret value.
+                    
+                    Current scores and game Leaderboards are displayed in your window.</p>
 
+                    <h4>End of the game</h4>
+                    <p>All good things must come to an end and so has your game 😞. It's okay, we've got you! Just click on create new to be redirected to the setup page where you can choose a different category or select play again to restart with the same setup.
+
+                    If you want to see the all time high scores then just click on the Leaderboards section to see where you stand.
+                    </p>
                 </div>
               </Popup>
 
