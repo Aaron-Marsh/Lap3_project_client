30662749bf84505a6e99fe202fd792db42bf88c5
nasiima <nasiima.mohamed1@gmail.com>
2022-06-27 12:27:18 +0100

    adding start quiz button

--- src/pages/Setup/index.js
+++ src/pages/Setup/index.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import CreateQuiz from '../../components/CreateQuiz/index';
+import { Box } from '@mui/system'
+import { Button } from '@mui/material'
 
 function Setup() {
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
+
+
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <CreateQuiz label="Catergory"/>
       <CreateQuiz label="Difficulty"/>
       <CreateQuiz label="Type"/>
+      <Box mt={3} width="50%">
+        <Button fullwidth variant='contained' type='submit' >
+          Start Quiz
+        </Button>
+        
+          </Box>
     </form>
 
 
