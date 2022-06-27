b785c9cbc3a63cbce85bf7165c46df943533b793
nasiima <nasiima.mohamed1@gmail.com>
2022-06-27 16:32:33 +0100

    handling all action types to  update the state when that action is dispatched

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,12 +2,37 @@ const initState = {
     question_category: "",
     question_difficulty: "",
     question_type: "",
-    amount_of_question: 10,
+    amount_of_question: 50,
     intScore: 0
  };
 
 const Reducer = (state=initState, action) => {
     switch(action.type){
+        case CHANGE_CATEGORY:
+      return {
+        ...state,
+        question_category: action.payload,
+      };
+      case CHANGE_DIFFICULTY:
+        return {
+          ...state,
+          question_difficulty: action.payload,
+        };
+      case CHANGE_TYPE:
+        return {
+          ...state,
+          question_type: action.payload,
+        };
+      case CHANGE_AMOUNT:
+        return {
+          ...state,
+          amount_of_question: action.payload,
+        };
+      case CHANGE_SCORE:
+        return {
+          ...state,
+          score: action.payload,
+        };
         case 'LOADING':
             return { ...state, location: action.payload, loading: true };
         case 'LOAD_RESULT':
