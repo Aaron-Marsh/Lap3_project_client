e0a345c4b6614c453e9cf3ba426b74d0633def12
Tom Millington <thomas.millington@mail.com>
2022-06-28 10:38:15 +0100

    remove leaderboard console logs

--- src/components/Leaderboard/index.js
+++ src/components/Leaderboard/index.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import podiumData from './data'
 import Podium from './Podium'
 
@@ -7,35 +7,30 @@ export default function Leaderboard() {
 
   async function getScores() {
     const options = {
-        method: 'GET'
+      method: 'GET'
     }
-    const result = await fetch(`https://lap3quizzer.herokuapp.com/scores`, options)
     try {
-        console.log(result);
+      const result = await fetch(`https://lap3quizzer.herokuapp.com/scores`, options)
+      let data = await result.json();
+      data = data.rows;
+      data = data.map((winner, position) => ({ ...winner, position }))
+
+      const trophyImgs = ['https://i.imgur.com/sRcjCDA.png', 'https://i.imgur.com/LZr8Arh.png', 'https://i.imgur.com/eGgxQKa.png']
+
+      for (let i = 0; i < 3; i++) {
+        let datum = data[i]
+        let trophy = trophyImgs[i]
+        data[i] = { ...datum, trophy }
+      }
+      setLeaderboard(data)
     } catch (error) {
-        console.error(error);
+      console.error(error);
     }
-
-    let data = await result.json();
-    data = data.rows;
-    data = data.map((winner, position) => ({ ...winner, position }))
-
-    const trophyImgs = ['https://i.imgur.com/sRcjCDA.png', 'https://i.imgur.com/LZr8Arh.png', 'https://i.imgur.com/eGgxQKa.png']
-
-    for (let i=0; i<3;i++){
-      let datum = data[i]
-      let trophy = trophyImgs[i]
-      data[i]={ ...datum, trophy }
-    }
-
-    setLeaderboard(data)
   }
 
-  useEffect(()=>{
+  useEffect(() => {
     getScores()
-
   }, [])
-  console.log(leaderboard);
 
   return (
     <div className="leaderboard">
