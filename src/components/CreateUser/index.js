e9b97187ec15ecab55bf1f12438d9d490bf071b0
GMillerMc <gmiller04@yahoo.com>
2022-06-28 16:13:09 +0100

    test welcome page

--- src/components/CreateUser/index.js
+++ src/components/CreateUser/index.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import styles from './index.module.css'
-import { useSelector, useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { handleUsernameChange } from '../../redux/action';
 
 
