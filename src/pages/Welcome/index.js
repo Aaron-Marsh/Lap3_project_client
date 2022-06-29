f83eb7295883be4d6e27d81972b9a33bd8667f61
GMillerMc <gmiller04@yahoo.com>
2022-06-29 14:26:28 +0100

    change to welcome page

--- src/pages/Welcome/index.js
+++ src/pages/Welcome/index.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import styles from './index.module.css'
 
 
+
 import { CreateUser, Modal } from '../../components';
 
 function Welcome() {
@@ -29,7 +30,7 @@ function Welcome() {
               </button>
               {openModal && <Modal closeModal={setOpenModal} />}
             </div>
-          
+              
           </div>
           
         </div> 
