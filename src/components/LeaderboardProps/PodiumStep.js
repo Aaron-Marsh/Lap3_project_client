61b1526ca9a6f1ecc0a5f7bff1f9741aa16fa941
Tom Millington <thomas.millington@mail.com>
2022-06-29 18:44:08 +0100

    add mobile view to leaderboards

--- src/components/LeaderboardProps/PodiumStep.js
+++ src/components/LeaderboardProps/PodiumStep.js
@@ -1,10 +1,12 @@
 import { motion } from 'framer-motion'
+import styles from './PodiumStep.module.css'
 
 export default function PodiumStep({ podium, winner }) {
   const offset = podium.length - winner.position
   if (winner.position <= 2) {
     return (
       <div
+        className={styles.upperPodium}
         style={{
           display: 'flex',
           flexDirection: 'column',
@@ -30,20 +32,22 @@ export default function PodiumStep({ podium, winner }) {
           }}
         >
           <img
+            className={styles.medals}
             src={`${winner.trophy}`}
             alt=""
             style={{
-              width: '2.75rem',
+              width: '3vw',
               overflow: 'hidden',
-              height: '2.75rem',
+              height: '3vw',
               borderRadius: 9999
             }}
           />
         </motion.div>
 
         <motion.div
+          className={styles.upperPodiumInner}
           style={{
-            width: '6rem',
+            width: '6vw',
             placeContent: 'center',
             display: 'grid',
             justifyContent: 'center',
@@ -52,8 +56,8 @@ export default function PodiumStep({ podium, winner }) {
             borderTopRightRadius: '.5rem',
             backgroundColor: '#26547C',
             marginBottom: -1,
-            filter: `opacity(${0.25 + offset / (podium.length+3)})`,
-            fontFamily: 'sans-serif'
+            filter: `opacity(${0.25 + offset / (podium.length + 3)})`,
+            fontFamily: 'var(--text-font)'
           }}
           initial="hidden"
           animate="visible"
@@ -70,61 +74,81 @@ export default function PodiumStep({ podium, winner }) {
             hidden: { opacity: 0, height: 0 }
           }}
         >
-          <p style={{ color: 'white', alignSelf: 'flex-end' }}>
+          <p className='podiumText' style={{ color: 'white', alignSelf: 'flex-end' }}>
             {winner.name}
           </p>
-          <p style={{ color: 'white', alignSelf: 'flex-end' }}>
+          <p className='podiumText' style={{ color: 'white', alignSelf: 'flex-end' }}>
             Score: {winner.score}
           </p>
         </motion.div>
       </div>
     )
-  } else{
-    return (
-      <div
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-          placeContent: 'center'
-        }}
-      > 
-        <motion.div
-          style={{
-            width: '6rem',
-            placeContent: 'center',
-            display: 'grid',
-            justifyContent: 'center',
-            justifyItems: 'center',
-            borderTopLeftRadius: '.5rem',
-            borderTopRightRadius: '.5rem',
-            backgroundColor: '#26547C',
-            marginBottom: -1,
-            filter: `opacity(${0.25 + offset / (podium.length+3)})`,
-            fontFamily: 'sans-serif'
-          }}
-          initial="hidden"
-          animate="visible"
-          variants={{
-            visible: {
-              height: 30 + 170 * (offset / podium.length),
-              opacity: 1,
-              transition: {
-                delay: offset,
-                duration: 2,
-                ease: 'backInOut'
-              }
-            },
-            hidden: { opacity: 0, height: 0 }
-          }}
-        >
-          <p style={{ color: 'white', alignSelf: 'flex-end' }}>
-            {winner.name}
-          </p>
-          <p style={{ color: 'white', alignSelf: 'flex-end' }}>
-            Score: {winner.score}
-          </p>
-        </motion.div>
-      </div>
-    )
+  } else {
+    if (document.querySelector('#mobilePodiumDiv')) {
+    const wrapper = document.querySelector('#mobilePodiumDiv');
+    if (!document.querySelector(`.podiumName${winner.position}`)){
+      let newName = document.createElement('p');
+      newName.setAttribute('class', `podiumName${winner.position}`);
+      let pos = parseInt(winner.position, 10) + 1;
+      newName.textContent = '#'+ pos +' - '+ winner.name;
+      wrapper.appendChild(newName);
+    }
+    
+    if (!document.querySelector(`.podiumScore${winner.position}`)){
+      let newScore = document.createElement('p');
+      newScore.setAttribute('class', `podiumScore${winner.position}`);
+      newScore.textContent = 'Score: '+winner.score;
+      wrapper.appendChild(newScore);
+    }
+
   }
+
+  return (
+    <div
+      style={{
+        flexDirection: 'column',
+        placeContent: 'center'
+      }}
+      className={styles.lowerPodium}
+    >
+      <motion.div
+        className='lowerPodiumInner'
+        style={{
+          width: '6vw',
+          placeContent: 'center',
+          display: 'grid',
+          justifyContent: 'center',
+          justifyItems: 'center',
+          borderTopLeftRadius: '.5rem',
+          borderTopRightRadius: '.5rem',
+          backgroundColor: '#26547C',
+          marginBottom: -1,
+          filter: `opacity(${0.25 + offset / (podium.length + 3)})`,
+          fontFamily: 'var(--text-font)'
+        }}
+        initial="hidden"
+        animate="visible"
+        variants={{
+          visible: {
+            height: 30 + 170 * (offset / podium.length),
+            opacity: 1,
+            transition: {
+              delay: offset,
+              duration: 2,
+              ease: 'backInOut'
+            }
+          },
+          hidden: { opacity: 0, height: 0 }
+        }}
+      >
+        <p className='podiumText' style={{ color: 'white', alignSelf: 'flex-end' }}>
+          {winner.name}
+        </p>
+        <p className='podiumText' style={{ color: 'white', alignSelf: 'flex-end' }}>
+          Score: {winner.score}
+        </p>
+      </motion.div>
+    </div>
+  )
+}
 }
